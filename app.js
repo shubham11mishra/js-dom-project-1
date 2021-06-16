f54928b06f29ec6d48725d1e24c9debeae871a4c
shubham mishra <shubham11mishra@gmail.com>
2021-06-16 20:50:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -242,19 +242,6 @@ productList.forEach(function (products) {
   cardDetail.appendChild(productPrice);
   card.appendChild(cardDetail);
 
-//   <a href="product.html?product_id=1"
-//   >
-//   <div class="img">
-//     <img
-//       src="https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg"
-//     />
-//   </div>
-//   <div class="details">
-//     <h3>Men Navy Blue Solid Sweatshirt</h3>
-//     <h4>United Colors of Benetton</h4>
-//     <h5>Rs 2599</h5>
-//   </div></a
-// >
   if (products.isAccessory) {
     accessoriesCardWapper.appendChild(card);
   } else {
